837acc4bf98353c85e5013ca81b278e782320ade
Krist <Kri2t@yandex.ru>
2022-08-30 20:09:35 +0300

    Выполнена 1 задача из д/з к семинару 1 (задача 8)

--- Introduction/Introduction.py
+++ Introduction/Introduction.py
@@ -1,3 +1,18 @@
-
 # -*- coding: cp1251 -*-
-print('привет мир!')
+x = float(input('x = '))
+y = float(input('y = '))
+
+if x > 0 and y > 0:
+    print('I четверть')
+elif x < 0 and y > 0:
+    print('II четверть')
+elif x < 0 and y < 0:
+    print('III четверть')
+elif x > 0 and y < 0:
+    print('IV четверть')
+elif x == 0 and y != 0:
+    print('Ось Y')
+elif x != 0 and y == 0:
+    print('Ось X')
+elif x == 0 and y == 0:
+    print('Центр')
