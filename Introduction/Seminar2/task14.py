92aa49216c27ae71bc0a9cd966032918b0dfd6aa
Krist <Kri2t@yandex.ru>
2022-09-01 02:35:58 +0300

    Небольшой фикс 14 задачи, чтобы считывалась цифра "9"

--- Introduction/Seminar2/task14.py
+++ Introduction/Seminar2/task14.py
@@ -5,7 +5,7 @@ summ : int = 0
 for x in range(0,len(numb)):
     if numb[x] == '.' or numb[x] == ',':
         pass
-    if ord('0') < ord(numb[x]) < ord('9'):
+    if ord('0') < ord(numb[x]) <= ord('9'):
         summ += int(numb[x])
 
 print(f'Сумма всех цифр в числе: {summ}')
