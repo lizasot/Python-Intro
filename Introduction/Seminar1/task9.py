eff2009aa83a94b4517f8bd02af186260f2fb27d
Krist <Kri2t@yandex.ru>
2022-08-31 16:15:39 +0300

    Исправление кодировки, где допустима кириллица + все задачи вынесены по разным файлам в отдельную папку

---
+++ Introduction/Seminar1/task9.py
@@ -0,0 +1,14 @@
+﻿print('Введите номер четверти прямоугольной системы координат: ')
+quarter = input()
+
+print('Допустимые значения: ')
+if ('3' in quarter) or ('III' in quarter):
+    print('x < 0; y < 0')
+elif ('2' in quarter) or ('II' in quarter):
+    print('x < 0; y > 0')
+elif ('1' in quarter) or ('I' in quarter):
+    print('x > 0; y > 0')
+elif ('4' in quarter) or ('IV' in quarter):
+    print('x > 0; y < 0')
+elif x == 0 and y != 0:
+    print('Ошибка! Не удалось распознать номер четверти при вводе.')
