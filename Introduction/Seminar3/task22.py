7d723ad27cf4de49e21db9126070516566405847
Krist <Kri2t@yandex.ru>
2022-09-06 14:17:16 +0300

    Выполнена задача 22

---
+++ Introduction/Seminar3/task22.py
@@ -0,0 +1,14 @@
+def summOddPos(l:list):
+    result = 0
+    for x in range(1,len(l),2):
+        result += l[x]
+    return result
+
+l = list(map(int, input('Введите через пробел числа: ').split()))
+test_l : list = [2, 3, 5, 9, 3]
+
+print(f'Ваш список чисел: {l}')
+print(f'Сумма чисел вашего списка на нечётных позициях: {summOddPos(l)}')
+
+print(f'Тестовый список чисел: {test_l}')
+print(f'Сумма чисел тестового списка на нечётных позициях: {summOddPos(test_l)}')
