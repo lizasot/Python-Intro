15dd8fa082f1dc503d55e3cef8d8d44c08bfc97e
Krist <Kri2t@yandex.ru>
2022-09-10 22:13:28 +0300

    Выполнена задача 33

---
+++ Introduction/Seminar4/task33.py
@@ -0,0 +1,25 @@
+from random import randint
+
+def getStrAXK(a : int, k : int):
+    if a == 0:
+        return ''
+    if a != 1 and k != 0:
+        result = (f'{a}*')
+    elif a != 1:
+        result = (f'{a}')
+    else:
+        result = ''
+    if k > 1:
+        result += (f'x^{k}')
+    elif k == 1:
+        result += 'x'
+    return result
+
+k = int(input('Введите число k, обозначающее степень многочлена: '))
+
+for x in range(k,-1,-1):
+    res = getStrAXK(randint(0,100),x)
+    if x != k and len(res) != 0:
+        print(' + ', end='')
+    print(res, end='')
+print()
