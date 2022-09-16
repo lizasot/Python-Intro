478db8b848cabb75fb3ccb415c952c103d96cef7
Krist <Kri2t@yandex.ru>
2022-09-16 17:20:00 +0300

    Завершено 80% игры и управления выбора

--- Introduction/Seminar5/task40.py
+++ Introduction/Seminar5/task40.py
@@ -1,5 +1,15 @@
 from time import sleep
-import keyboard
+import keyboard, enum, os
+
+def clear():
+    #os.system('cls' if os.name=='nt' else 'clear')
+    print('\n'*100)
+
+class Direction (enum.Enum):
+    left = 1
+    right = 2
+    up = 3
+    down = 4
 
 def getStrField(field : list):
     if len(field) != 9:
@@ -12,16 +22,16 @@ def getStrField(field : list):
 def checkWin(field : list):
     #по горизонтали
     for x in range(0,9,3):
-        if field[x+0]!= ' ' and field[x+1]!= ' ' and field[x+2] != ' ':
+        if field[x+0] == field[x+1] == field[x+2] != ' ':
             return True
     #по вертикали
     for x in range(0,3):
-        if field[x+0]!= ' ' and (field[x+3] == field[x+6]):
+        if field[x+0] == field[x+3] == field[x+6] != ' ':
             return True
     #по диагонали
-    if field[0] != ' ' and (field[0] == field[4] == field[8]):
+    if field[0] == field[4] == field[8] != ' ':
         return True
-    if field[2] != ' ' and (field[2] == field[4] == field[6]):
+    if field[2] == field[4] == field[6] != ' ':
         return True
     return False
 
@@ -30,35 +40,103 @@ def drawSelectCell(field : list, ind : int):
         return ''
     if ind < 0 or ind >= 9:
         return ''
-    new_field = field
+    new_field= list.copy(field)
     new_field[ind] = '*'
     return getStrField(new_field)
 
 def getIndFirstFreeCeil(field : list):
     for x in range(0,len(field)):
-        if field[x]:
+        if field[x] == ' ':
             return x
     return -1
+
+def updateConsole(player : bool, field : list, current_cell : int):
+    if player:
+        print('Ходит игрок 2') #'o'
+    else:
+        print('Ходит игрок 1') #'x'
+    print(drawSelectCell(field,current_cell))
+
+def getFreeCell(field : list):
+    result : list = []
+    for x in range(0,9):
+        if field[x] == ' ':
+            result.append(x)
+    return result
+
+def moveSelection(field : list, current_cell : int, direction : Direction):
+    freeCell = getFreeCell(field)
+    if direction == Direction.left:
+        if current_cell == 0:
+            return 8
+        else:
+            return current_cell
+    elif direction == Direction.right:
+        if current_cell != 2 and current_cell != 5 and current_cell != 8:
+            if (current_cell + 1) in freeCell:
+                return current_cell + 1
+            elif (current_cell + 2) in freeCell:
+                return current_cell + 2
+            else:
+                return current_cell
+        else:
+            return current_cell
+    elif direction == Direction.up:
+        if current_cell != 0 and current_cell != 1 and current_cell != 2:
+            if (current_cell - 3) in freeCell:
+                return current_cell - 3
+            elif (current_cell - 6) in freeCell:
+                return current_cell - 6
+            else:
+                return current_cell
+        else:
+            return current_cell
+    elif direction == Direction.down:
+        if current_cell != 6 and current_cell != 7 and current_cell != 8:
+            if (current_cell + 3) in freeCell:
+                return current_cell + 3
+            elif (current_cell + 6) in freeCell:
+                return current_cell + 6
+            else:
+                return current_cell
+        else:
+            return current_cell
 
 field : list = [' ']*9
 player = False
 
 while True:
-    if player:
-        print('Ходит игрок 2') #'o'
-    else:
-        print('Ходит игрок 1') #'x'
     current_cell = getIndFirstFreeCeil(field)
-    print(drawSelectCell(field,current_cell))
+    clear()
+    updateConsole(player, field, current_cell)
+    sleep(0.8)
     key = keyboard.read_key()
-    keyboard.on_release_key(key, callback)
     while key != 'enter':
         if key == 'left':
-            print('press left')
+            current_cell = moveSelection(field,current_cell,Direction.left)
         elif key == 'right':
-            print('press right')
+            current_cell = moveSelection(field,current_cell,Direction.right)
         elif key == 'up':
-            print('press up')
+            current_cell = moveSelection(field,current_cell,Direction.up)
         elif key == 'down':
-            print('press down')
+            current_cell = moveSelection(field,current_cell,Direction.down)
+        clear()
+        updateConsole(player, field, current_cell)
+        sleep(0.2)
         key = keyboard.read_key()
+    if player:
+        field[current_cell] = 'o'
+    else:
+        field[current_cell] = 'x'
+
+    if checkWin(field):
+        break
+    else:
+        player = not player
+
+
+if player:
+    print('Победил игрок 2!') #'o'
+else:
+    print('Победил игрок 1!') #'x'
+print(getStrField(field))
