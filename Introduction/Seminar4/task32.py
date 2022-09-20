55a93d36e966378a17ebb671dbece74f42bdea89
Krist <Kri2t@yandex.ru>
2022-09-20 13:16:56 +0300

    Откорректирована задача 32: теперь выводится список чисел, которые повторяются только единожды

--- Introduction/Seminar4/task32.py
+++ Introduction/Seminar4/task32.py
@@ -1,8 +1,10 @@
 l = list(map(int, input('Введите через пробел числа: ').split()))
 result : list = []
+print(f'{l} => ', end='')
 
+l.sort()
 for x in range(0,len(l)):
-    if l[x] not in result:
+    if not (l[x] == l[x-1] or (x != len(l) -1 and l[x] == l[x+1])):
         result.append(l[x])
 
-print(f'{l} => {result}')
+print(f'{result}')
