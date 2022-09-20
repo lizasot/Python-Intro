0185d63d1c42e28a9ed65f93bb7566affbd5557a
Krist <Kri2t@yandex.ru>
2022-09-21 00:30:57 +0300

    Рефакторинг кода задачи 12

--- Introduction/Seminar2/task12.py
+++ Introduction/Seminar2/task12.py
@@ -1,7 +1,3 @@
 n = int(input('Введите значение n, т.е. кол-во элементов словаря: '))
-
-d : dict = {}
-
-for x in range(n):
-    d[x + 1]  = (x + 1) * 3 + 1
+d : dict = dict(enumerate([(x + 1) * 3 + 1 for x in range(n)], start=1))
 print(f'Получившийся словарь: {d}')
