b8318df8ae4f9d87527dca902e8a277ee7f15107
Krist <Kri2t@yandex.ru>
2022-09-20 13:42:16 +0300

    Упрощение кода по задаче 38

--- Introduction/Seminar5/task38.py
+++ Introduction/Seminar5/task38.py
@@ -1,13 +1,10 @@
 def deleteABV(text : str):
-    y = 0
-    result = ''
-    for x in range(0,len(text)+1):
-        if x == len(text) or text[x] == ' ' :
-            if 'абв' not in text[y:x]:
-                result += text[y:x]
-            y = x
-    result = result.strip()
-    return result
+    l = text.split()
+    result: list = []
+    for x in range(0,len(l)):
+        if 'абв' not in l[x]:
+            result.append(l[x])
+    return ' '.join(result)
 
 text = input('Введите текст: ')
 print('Удаляются все слова, содержащие "абв"...')
