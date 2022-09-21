a265db730bd677bc5c13597e9e37218373884656
Krist <Kri2t@yandex.ru>
2022-09-21 15:12:22 +0300

    Убран лишний код

--- Introduction/Seminar2/task16.py
+++ Introduction/Seminar2/task16.py
@@ -1,10 +1,4 @@
 from functools import reduce
-
-def calculate(n : int):
-    result = (1 + 1/n)**n
-    return result
-
 n = int(input('Введите число N: '))
 summ = reduce(lambda x, y: x + y, [(1 + 1/n)**n for n in range(1, n + 1)])
-
 print(summ)
