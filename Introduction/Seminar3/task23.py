42888c3523d568eefee7710e22a0616f1d3b2a03
Krist <Kri2t@yandex.ru>
2022-09-22 13:29:09 +0300

    Рефакторинг кода 32 задачи

--- Introduction/Seminar3/task23.py
+++ Introduction/Seminar3/task23.py
@@ -1,11 +1,7 @@
-def multiplyPairs(l:list):
-    result : list = []
-    for x in range(0,round(len(l) / 2 + 0.1)):
-        result.append(l[x]*l[(len(l) - 1) - x])
-    return result
-
 l = list(map(int, input('Введите через пробел числа: ').split()))
 test_l : list = [2, 3, 4, 5, 6]
+
+multiplyPairs = lambda l:[l[x]*l[(len(l) - 1) - x] for x in range(0,round(len(l) / 2 + 0.1))]
 
 print(f'Ваш список чисел: {l}')
 print(f'Произведение пар чисел вышего списка: {multiplyPairs(l)}')
--- Introduction/Seminar4/task32.py
+++ Introduction/Seminar4/task32.py
@@ -1,8 +1,10 @@
 l = list(map(int, input('Введите через пробел числа: ').split()))
 result : list = []
+print(f'{l} => ', end='')
 
-for x in range(0,len(l)):
-    if l[x] not in result:
-        result.append(l[x])
+check = lambda x: True if not (l[x] == l[x-1] or (x != len(l) -1 and l[x] == l[x+1])) else False
 
-print(f'{l} => {result}')
+l.sort()
+result = [l[x] for x in range(0,len(l)) if check(x)] 
+
+print(f'{result}')
