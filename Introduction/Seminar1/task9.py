c6346e0e48ce2ff9a8d6c320faa62f2e72554843
Krist <Kri2t@yandex.ru>
2022-09-22 14:01:20 +0300

    Исправление 9 задачи

--- Introduction/Seminar1/task9.py
+++ Introduction/Seminar1/task9.py
@@ -10,5 +10,5 @@ elif ('1' in quarter) or ('I' in quarter):
     print('x > 0; y > 0')
 elif ('4' in quarter) or ('IV' in quarter):
     print('x > 0; y < 0')
-elif x == 0 and y != 0:
+else:
     print('Ошибка! Не удалось распознать номер четверти при вводе.')
